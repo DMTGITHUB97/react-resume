dc5edeb904cf6f0981b2570679be83961754ab4b
DMTGIT <dmtyagigithub1997@gmail.com>
2023-11-25 15:50:24 +0530

    test files commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { React } from 'react';
-import './Components.css';
+
 
 function App() {
   return (
---
+++ src/components/data.js
@@ -0,0 +1,18 @@
+const data = {
+    skills: ["January", "February", "March", "April", "May", "June"],
+    summary: [
+        'A curious learner and creative team player, interesting in the job role of Software Developer where I can utilise my understanding of coding and software development to efficiently fulfil the requirements of clients and customers.'
+    ],
+    personalDetail: [
+        {
+            address: [
+                {
+                    street: "123, ABC Street",
+                    city: "Hyderabad",
+                    state: "Telangana",
+                    country: "India"
+                }
+            ]
+        }
+    ]
+}
